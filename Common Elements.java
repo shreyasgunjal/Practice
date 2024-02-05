1047ad7613d0538a2585ab465056bd1a9e498d84
Shreyas Ramdas Gunjal <157288244+shreyasgunjal@users.noreply.github.com>
2024-02-05 19:03:31 +0530

    Update Common Elements.java

--- Common Elements.java
+++ Common Elements.java
@@ -3,15 +3,37 @@
 class Solution{
     public static ArrayList<Integer> common_element(int v1[], int v2[])
     {
-        //Your code here
+        //Brute-Force Approach or Naive
+        // ArrayList<Integer> arr = new ArrayList<Integer>();
+        // for(int i =0; i<v1.length; i++){
+        //     int sh = v1[i];
+        //     for(int j=0; j<v2.length; j++){
+        //         int at = v2[j];
+        //         if(sh == at){
+        //             arr.add(sh);
+        //         }
+        //     }
+        // }
+        // return arr;
+
+
+        //Two_pointer Approach
         ArrayList<Integer> arr = new ArrayList<Integer>();
-        for(int i =0; i<v1.length; i++){
-            int sh = v1[i];
-            for(int j=0; j<v2.length; j++){
-                int at = v2[j];
-                if(sh == at){
-                    arr.add(sh);
-                }
+        
+        Arrays.sort(v1);
+        Arrays.sort(v2);
+        int i=0;
+        int j=0;
+        
+        while(i<v1.length && j<v2.length){
+            if(v1[i] == v2[j]){
+                arr.add(v1[i]);
+                i++;
+                j++;
+            }else if(v1[i]<v2[j]){
+                i++;
+            }else{
+                j++;
             }
         }
         return arr;
